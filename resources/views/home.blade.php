908d9adc3ac5240c214e33c401fbd1454175ec73
Chinmay anand <chinmayanand896@gmail.com>
2020-02-04 01:28:30 +0530

    UI bug fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="container text-center">
+    <h2>Products</h2>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="row">
+        @foreach($products as $product)
+            <div class="col-4 p-2">
+                <div class="card p-2">
+                    <img class="card-img-top" src="{{ asset('default-product.png') }}" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $product->name }}</h4>
+                        <p class="card-text">{{ $product->description }}</p>
+                    </div>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Add to cart</a>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
 </div>
 @endsection
